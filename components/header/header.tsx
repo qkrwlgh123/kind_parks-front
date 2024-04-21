7f966554c9c11f016951ea17a66626f355827f67
qkrwlgh123 <jiho1024@naver.com>
2024-04-21 22:36:06 +0900

    로그인 구현

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,11 +1,21 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./header.module.css";
+import { useRouter } from "next/navigation";
 
 export default function Header({
   handleChangeSideBarStatus,
 }: {
   handleChangeSideBarStatus: () => void;
 }) {
+  const router = useRouter();
+  const token = localStorage.getItem("parks_token");
+  /** 로그아웃 함수 */
+  const handleRequestLogout = () => {
+    localStorage.removeItem("parks_token");
+    router.refresh();
+  };
   return (
     <header className={styles.header}>
       <div className={styles.inner__container}>
@@ -14,10 +24,16 @@ export default function Header({
         </div>
         <div className={styles.menus__container}>
           <h3 onClick={handleChangeSideBarStatus}>Parks</h3>
-          <h3>글 작성하기</h3>
-          <Link href="/login" className={styles.link__style}>
-            <h3>로그인</h3>
-          </Link>
+
+          {token && <h3>글 작성하기</h3>}
+
+          {token ? (
+            <h3 onClick={handleRequestLogout}>로그아웃</h3>
+          ) : (
+            <Link href="/login" className={styles.link__style}>
+              <h3>로그인</h3>
+            </Link>
+          )}
         </div>
       </div>
     </header>
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,44 @@
+"use client";
+
+import { useState } from "react";
 import styles from "./page.module.css";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
+  const router = useRouter();
+  /** 로그인 요청 정보 */
+  const [loginInfo, setLoginInfo] = useState({ id: "", password: "" });
+
+  /** 로그인 요청 함수 */
+  const handleLoginRequest = async (
+    event: any,
+    id: string,
+    password: string
+  ) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://15.164.166.107:4000/api/auth/login",
+        {
+          id: id,
+          password: password,
+        }
+      );
+
+      // 로그인 성공 시 처리
+      const token = response.data.token;
+      localStorage.setItem("parks_token", token);
+      router.replace("/");
+    } catch (error: any) {
+      // 로그인 실패 시 처리
+      console.error("로그인 실패:", error.response.data);
+      alert("계정정보를 확인해주세요.");
+      throw error; // 예외를 다시 던져서 호출하는 부분에서 처리하도록 합니다.
+    }
+  };
+
   return (
     <main className={styles.login__box}>
       <div>
@@ -8,11 +46,31 @@ export default function Login() {
       </div>
       <form className={styles.form__box}>
         <div className={styles.inputs__container}>
-          <input className={styles.input} placeholder="계정" />
-          <input className={styles.input} placeholder="비밀번호" />
+          <input
+            className={styles.input}
+            placeholder="계정"
+            value={loginInfo.id}
+            onChange={(event) => {
+              setLoginInfo({ ...loginInfo, id: event.target.value });
+            }}
+          />
+          <input
+            className={styles.input}
+            placeholder="비밀번호"
+            type="password"
+            value={loginInfo.password}
+            onChange={(event) => {
+              setLoginInfo({ ...loginInfo, password: event.target.value });
+            }}
+          />
         </div>
 
-        <button className={styles.submit__btn}>
+        <button
+          className={styles.submit__btn}
+          onClick={(event) =>
+            handleLoginRequest(event, loginInfo.id, loginInfo.password)
+          }
+        >
           <span>로그인</span>
         </button>
       </form>
