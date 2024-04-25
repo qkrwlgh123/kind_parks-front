7be0044cfc7da012ef1f10612e7963dc64593cf3
qkrwlgh123 <jiho1024@naver.com>
2024-04-26 00:34:54 +0900

    하위메뉴 등록 추가

--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -10,7 +10,10 @@ export default function Sidebar({
   isSidebarActive: boolean;
 }) {
   const [menuList, setMenuList] = useState<string[]>([]);
-  const fetchFunc = async () => {
+  const [submenuList, setSubmenuList] = useState<string[]>([]);
+  const [selectedMenu, setSelectedMenu] = useState<string | null>(null);
+
+  const fetchMenulistFunc = async () => {
     const response = await axios.get(
       "https://server.kindparks.com/api/menu/view"
     );
@@ -18,9 +21,34 @@ export default function Sidebar({
       setMenuList(response.data.data);
     }
   };
+
+  const fetchSubmenuFunc = async (menu: string) => {
+    const response = await axios.post(
+      `https://server.kindparks.com/api/menu/submenu/view`,
+      {
+        parentMenu: menu,
+      }
+    );
+    if (response.data.code === 200) {
+      setSubmenuList(response.data.data);
+    }
+  };
+
+  const handleMenuClick = async (menu: string) => {
+    if (selectedMenu === menu) {
+      // 이미 선택된 메뉴를 다시 클릭하면 하위 메뉴 리스트를 숨깁니다.
+      setSelectedMenu(null);
+      setSubmenuList([]);
+    } else {
+      setSelectedMenu(menu);
+      fetchSubmenuFunc(menu);
+    }
+  };
+
   useEffect(() => {
-    fetchFunc();
+    fetchMenulistFunc();
   }, []);
+  /** 클릭 시, 해당 메뉴에 해당하는 subMenulist를 불러와서 li 태그 아래에 붙여야한다.*/
   return (
     <div
       className={`${styles.sidebar__container} ${
@@ -30,7 +58,20 @@ export default function Sidebar({
       <ul className={styles.sidebar__list}>
         {menuList?.map((item: any) => (
           <div key={item.id}>
-            <span>{item.menu}</span>
+            <li>
+              <span onClick={() => handleMenuClick(item.menu)}>
+                {item.menu}
+              </span>
+              {selectedMenu === item.menu && submenuList.length > 0 && (
+                <ul className={styles.submenu__list}>
+                  {submenuList?.map((submenu: any, index: number) => (
+                    <li key={index}>
+                      <span>{submenu.name}</span>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
           </div>
         ))}
       </ul>
--- src/app/addMenu/page.tsx
+++ src/app/addMenu/page.tsx
@@ -6,9 +6,11 @@ import axios from "axios";
 
 export default function AddMenu() {
   const [inputedText, setInputedText] = useState("");
-
+  const [inputedSubmenuText, setInputedSubmenuText] = useState("");
   const [menuList, setMenuList] = useState<string[]>([]);
 
+  const [submenuList, setSubmenuList] = useState<string[]>([]);
+  const [selectedMenu, setSelectedMenu] = useState<string>("");
   const fetchFunc = async () => {
     const response = await axios.get(
       "https://server.kindparks.com/api/menu/view"
@@ -18,8 +20,33 @@ export default function AddMenu() {
     }
   };
 
+  const fetchSubmenulistFunc = async (menu: string) => {
+    const response = await axios.post(
+      "https://server.kindparks.com/api/menu/submenu/view",
+      {
+        parentMenu: menu,
+      }
+    );
+    if (response.data.code === 200) {
+      setSubmenuList(response.data.data);
+    }
+  };
+  const handleMenuClick = async (menu: string) => {
+    if (selectedMenu === menu) {
+      // 이미 선택된 메뉴를 다시 클릭하면 하위 메뉴 리스트를 숨깁니다.
+      setSelectedMenu(null);
+      setSubmenuList([]);
+    } else {
+      setSelectedMenu(menu);
+      fetchSubmenulistFunc(menu);
+    }
+  };
   const handleInputText = (value: string) => {
     setInputedText(value);
+  };
+
+  const handleSubmenuInputText = (value: string) => {
+    setInputedSubmenuText(value);
   };
 
   const handleSubmitText = async () => {
@@ -39,40 +66,100 @@ export default function AddMenu() {
     }
   };
 
+  const handleSubmitSubmenuText = async () => {
+    const response = await axios.post(
+      "https://server.kindparks.com/api/menu/submenu/add",
+      {
+        text: inputedSubmenuText,
+        parentMenu: selectedMenu,
+      }
+    );
+
+    if (response.data.code === 200) {
+      const resultText = inputedSubmenuText;
+      alert("등록되었습니다.");
+      setInputedSubmenuText("");
+      setSubmenuList([...submenuList, resultText]);
+      fetchSubmenulistFunc(selectedMenu);
+    }
+  };
+
   useEffect(() => {
     fetchFunc();
   }, []);
-  console.log(menuList);
+
   return (
     <main className={styles.login__box}>
       <div>
         <h1>사이드바 메뉴 편집</h1>
       </div>
-      <div>
-        <div>현재 메뉴 리스트</div>
-        <div>
-          {menuList?.map((item: any) => (
-            <div key={item.id}>
-              <span>{item.menu}</span>
+      <div className={styles.inner__container}>
+        <div className={styles.left__container}>
+          <div>
+            <div>상위 메뉴 리스트</div>
+            <div>
+              <span>(메뉴를 클릭하면 하위메뉴가 나와요 !)</span>
             </div>
-          ))}
+            <div>
+              {menuList?.map((item: any) => (
+                <div
+                  key={item.id}
+                  className={styles.menu__box}
+                  onClick={() => handleMenuClick(item.menu)}
+                >
+                  <span className={styles.menu__btn}>{item.menu}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className={styles.input__box}>
+            <div>
+              <input
+                type="text"
+                value={inputedText}
+                className={styles.input}
+                onChange={(e) => handleInputText(e.target.value)}
+              />
+            </div>
+            <button
+              className={styles.submit__btn}
+              onClick={(e) => handleSubmitText()}
+            >
+              <span>상위 메뉴 추가</span>
+            </button>
+          </div>
         </div>
-      </div>
-      <div className={styles.input__box}>
         <div>
-          <input
-            type="text"
-            value={inputedText}
-            className={styles.input}
-            onChange={(e) => handleInputText(e.target.value)}
-          />
+          <div>
+            <div>하위 메뉴 리스트</div>
+            <div>
+              <span>(메뉴를 클릭하면 하위메뉴가 나와요 !)</span>
+            </div>
+            <div>
+              {submenuList?.map((item: any) => (
+                <div key={item.id} className={styles.menu__box}>
+                  <span className={styles.menu__btn}>{item.name}</span>
+                </div>
+              ))}
+            </div>
+            <div className={styles.input__box}>
+              <div>
+                <input
+                  type="text"
+                  value={inputedSubmenuText}
+                  className={styles.input}
+                  onChange={(e) => handleSubmenuInputText(e.target.value)}
+                />
+              </div>
+              <button
+                className={styles.submit__btn}
+                onClick={(e) => handleSubmitSubmenuText()}
+              >
+                <span>하위 메뉴 추가</span>
+              </button>
+            </div>
+          </div>
         </div>
-        <button
-          className={styles.submit__btn}
-          onClick={(e) => handleSubmitText()}
-        >
-          <span>추가하기</span>
-        </button>
       </div>
     </main>
   );
